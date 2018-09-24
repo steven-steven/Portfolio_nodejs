918ec170ed8bc7d27641654d94f9b9e755b6b2f6
Steven Steven <steven.infinity29@gmail.com>
2018-09-24 13:00:14 -0400

    Converted Processing Project Repo to a general Portfolio for Winter 2019 coop application

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
     var pathname = window.location.pathname;
-    $('.nav > li > a[href="'+pathname+'"]').addClass('active');
+    var activeList = $('#sidebar a[href="'+pathname+'"]').parent()
+    activeList.addClass('active');
+    if(activeList.parent().hasClass("collapse")){
+        activeList.parent().collapse("show");
+    }
 });
--- server/server.js
+++ server/server.js
@@ -22,26 +22,50 @@ app.listen( port, ()=>{console.log(`Server up on port ${port}`)});
 //handle page requests
 app.get('/', (req, res)=>{
     res.render('index.hbs', {
-        pageTitle: "About Page",
-        contents: "Navigate through my sketch projects"
+        pageTitle: "Welcome. Know more about me!",
+        contents: "Note: This site is currently in progress. I am conveting the old site (which uses static html/css/js) to use the Node.js/Express framework."
+    });
+});
+
+app.get('/resume', (req, res)=>{
+    res.render('resume.hbs', {
+        pageTitle: "Resume"
+    });
+});
+app.get('/coop', (req, res)=>{
+    res.render('coop.hbs', {
+        pageTitle: "Work Experiences"
+    });
+});
+app.get('/clubs', (req, res)=>{
+    res.render('clubs.hbs', {
+        pageTitle: "Clubs and Activities"
+    });
+});
+app.get('/contacts', (req, res)=>{
+    res.render('contacts.hbs', {
+        pageTitle: "Contact Me"
     });
 });
 
 app.get('/breaker', (req, res)=>{
     res.render('processing.hbs', {
         pageTitle: "Brick Breaker",
+        github: "github.com/steven-steven/Breaker_Game_Processing",
         processingPdeFile: "processing_files/breaker/breaker.pde"
     });
 });
 app.get('/tictactoe', (req, res)=>{
     res.render('processing.hbs', {
         pageTitle: "Tic Tac Toe",
+        github: "github.com/steven-steven/TicTacToe",
         processingPdeFile: "processing_files/tictactoe/tictactoe.pde"
     });
 });
 app.get('/insertionSort', (req, res)=>{
     res.render('processing_sort.hbs', {
         pageTitle: "Insertion Sort",
+        github: "github.com/steven-steven/Sorting_Sketch",
         processingPdeFile: "processing_files/sorting/sorting.pde"
     });
 });
